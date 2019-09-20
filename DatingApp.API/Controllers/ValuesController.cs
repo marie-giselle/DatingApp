476a16452bc35ed2c4aa3f6b2f22021fee0666a9
YKirillova <YKirillova@mlg.ru>
2019-09-20 14:35:16 +0300

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
